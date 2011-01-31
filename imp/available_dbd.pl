b468e2d14fef817fbbd3ae1d73f0b2e60b7337a0
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2011-01-31 22:47:25 +0100

    Remove unneeded file

--- imp/available_dbd.pl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/perl
-use DBI;
-@driver_names = DBI->available_drivers();
-print "driver_names (apparently installed, available):\n";
-foreach my $dn (@driver_names)
-{
-    print "$dn\n";
-}
