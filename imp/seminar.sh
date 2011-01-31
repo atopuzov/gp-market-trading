c366ec9987984fbc53d7a5d8f3c1ec95d87511e7
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2011-01-31 22:48:04 +0100

    Kreira bazu koristenu u semniaru

---
+++ imp/seminar.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Dionice obradjene u seminaru
+./import-from-zse.rb -t ATPL-R-A
+./import-from-zse.rb -t BD62-R-A
+./import-from-zse.rb -t DLKV-R-A
+./import-from-zse.rb -t ERNT-R-A
+./import-from-zse.rb -t FRNK-R-A
+./import-from-zse.rb -t IGH-R-A
+./import-from-zse.rb -t INDG-R-A
+./import-from-zse.rb -t KOEI-R-A
+./import-from-zse.rb -t LEDO-R-A
+./import-from-zse.rb -t PBZ-R-A
+./import-from-zse.rb -t TISK-R-A
+./import-from-zse.rb -t VDKT-R-A
+./import-from-zse.rb -t ZABA-R-A
