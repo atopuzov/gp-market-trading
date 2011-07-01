2827ed52da2f9164b956ba10306d9a009f68bde9
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2011-07-01 15:55:50 +0200

    Update individual run

--- individual.cpp
+++ individual.cpp
@@ -5,6 +5,7 @@
 #include "beagle/GP.hpp"
 #include "eval.hpp"
 #include "Primitives.hpp"
+#include "Fitness.hpp"
 
 #define _USE_MATH_DEFINES
 #include <cmath>
@@ -21,16 +22,16 @@ using namespace GP;
 
 int main(int argc, char *argv[]) {
 	try {
-		// 1: Kreiraj skup primitiva
-		GP::PrimitiveSet::Handle lSet = new GP::PrimitiveSet(&typeid(Bool));	// STGP, korjenski cvor Bool
+		// 1. Build primitives.
+		GP::PrimitiveSet::Handle lSet = new GP::PrimitiveSet(&typeid(Bool));	// STGP, root node type Bool
 		
-		AddPrimitives(lSet);	// Dodaj primitive
+		AddPrimitives(lSet);	// Add primitives
 		
-		// 2: Kreiraj sustav
-		aco::Context::Alloc::Handle lContextAlloc = new aco::Context::Alloc();
+		// 2. Build a system.
+		trading::Context::Alloc::Handle lContextAlloc = new trading::Context::Alloc();
 		GP::System::Handle lSystem = new GP::System(lSet, lContextAlloc);
 		
-		// 3: Evaluacijski operator
+		// 3. Evaluation operator
 		eval::Handle lEvalOp = new eval;
 
 		// Initialize operators and system
@@ -38,34 +39,31 @@ int main(int argc, char *argv[]) {
 		lSystem->initialize(argc, argv);
 		lSystem->postInit();
 		lEvalOp->postInit(*lSystem);
-		std::string lFileName = "best.xml"; 		// Ucitaj najboljeg
+		std::string lFileName = "best.xml"; 		// Load the best individual
 		
-
-		// Log trgovanja
-		//Beagle::Bool::Handle logTrgovanja = castHandleT<Beagle::Bool>(lSystem->getRegister()["aco.log"]);
-		//logTrgovanja->setWrappedValue(true);
-		
-		Beagle::GP::Individual::Handle lIndividual = new Beagle::GP::Individual;
+		GP::Tree::Alloc::Handle lTreeAlloc = new GP::Tree::Alloc;
+		trading::Fitness::Alloc::Handle lFitnessAlloc = new trading::Fitness::Alloc;	// Fitness
+		Beagle::GP::Individual::Handle lIndividual = new Beagle::GP::Individual(lTreeAlloc,lFitnessAlloc);
 		lIndividual->readFromFile(lFileName.c_str(), *lSystem);
 		
 	    // Write individual to screen
 		//std::cout << std::endl << "Read individual: " << lIndividual->serialize() << std::endl;
 	
 	    // Evaluate individual
-	    Beagle::Fitness::Handle lFitness = lEvalOp->test(lIndividual, lSystem);
+	    trading::Fitness::Handle lFitness = castHandleT<trading::Fitness>(lEvalOp->test(lIndividual, lSystem));
 	
 	    // Write fitness to screen
-	    //std::cout << "Fitness: " << std::endl;
-	    //std::cout << lFitness->serialize() << std::endl;
+	    std::cout << "Fitness: " << std::endl;
+	    std::cout << lFitness->getTrainingSetValue() << std::endl;
 
 	} catch(Exception& inException) {
 		inException.terminate();
 	} catch(exception& inException) {
-		cerr << "Uhvacena standardna iznimka:" << endl;
+		cerr << "Cought a standard exception:" << endl;
 		cerr << inException.what() << endl << flush;
 		return 1;
 	} catch(...) {
-		cerr << "Uhvacena nepoznata iznimka!" << endl << flush;
+		cerr << "Cought an unknown exception!" << endl << flush;
 		return 1;
 	}
   return 0;
