c7d93747aa9aea37759676f1b1646b1a73d3181c
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2011-07-04 12:17:31 +0200

    Remove old visualizer batch file

--- v.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-DEL viz\*.html viz\*.svg viz\*.obm viz\*.log viz\*.csv
-REM utils\gzip -df beagle.obm.gz
-REM utils\beagleviz -b http://localhost/visualizer -o viz beagle.obm beagle.log
-utils\beagleviz -o viz beagle.obm beagle.log
-start viz\index.html
