012ba7ca88901ba38359221a18376049657cedcb
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2011-07-08 14:37:08 +0200

    Code refactoring

--- ROC.cpp
+++ ROC.cpp
@@ -2,9 +2,10 @@
  * (c) 2008-2011 Aleksandar Topuzović                                      *
  * <aleksandar.topuzovic@fer.hr>, <aleksandar.topuzovic@gmail.com>         *
  ***************************************************************************/
-#include "beagle/GP.hpp"
+#include <beagle/GP.hpp>
 #include "ROC.hpp"
 #include "util.hpp"
+#include "ta.hpp"
 
 #include <sstream>
 #include <cmath>
@@ -12,10 +13,9 @@
 
 using namespace Beagle;
 
-// Ime ROC
-ROC::ROC() :
-  Beagle::GP::Primitive(1, "ROC")
-{ }
+// Name ROC
+ROC::ROC() : Beagle::GP::Primitive(1, "ROC")
+{	}
 
 #ifdef BEAGLE_HAVE_RTTI
 
@@ -34,7 +34,7 @@ const std::type_info* ROC::getReturnType(Beagle::GP::Context& ioContext) const
 
 #endif // BEAGLE_HAVE_RTTI
 
-// Vraca srednju vrjednost dionice kroz period
+// Calculates the rate of change (ROC)
 void ROC::execute(GP::Datum& outDatum, GP::Context& ioContext)
 {
 	Double& lResult = castObjectT<Double&>(outDatum);
@@ -42,47 +42,9 @@ void ROC::execute(GP::Datum& outDatum, GP::Context& ioContext)
 	get1stArgument(lArg, ioContext); 
 	trading::Context& aContext = castObjectT<trading::Context&>(ioContext);
 
-	sqlite3 *database;		// Baza podataka  
-	database = aContext.database;
-
-	std::ostringstream interval;
-	interval << lArg;
-
-	// Closing price n days ago
-	string sql = "SELECT ZADNJA FROM ZSE WHERE DIONICA=UPPER('";
-	sql       += aContext.dionica;				// Dionica
-	sql       += "') AND DATUM BETWEEN DATE('";
-	sql       += aContext.datum;
-	sql       += "','-"; 
-	sql       += interval.str();				// Vremenski interval
-	sql       += " day') AND DATE('";
-	sql       += aContext.datum;				// Trenutni datum
-	sql       += "') ORDER BY DATUM ASC LIMIT 1"; 
-	// Vrati interval, pa koristi najblizi dan (ukoliko odbarani dan ispadne neradni, nema rezultata)
- 
-	// Closing price of current day
-	string sql2 = "SELECT ZADNJA FROM ZSE WHERE DIONICA=UPPER('";
-	sql2       += aContext.dionica;				// Dionica
-	sql2       += "') AND DATUM = DATE('";
-	sql2       += aContext.datum;
-	sql2       += "')"; 
-
-
-	//std::cout << sql << "\n\n" << sql2 << std::endl;
-
-	sqlite3_stmt *preparedStatement;
-	SQLITE_PREPARE_STATEMENT_TEST(preparedStatement, sql, database);
-	SQLITE_STEP_TEST(preparedStatement);
-	double close_ndaysago = sqlite3_column_double(preparedStatement,0);	// Closing price n days ago
-	sqlite3_finalize(preparedStatement);
+	double close_ndaysago = price_days_ago(aContext.database, aContext.dionica, aContext.datum, lArg);
+	double close_today = price_on_day(aContext.database, aContext.dionica, aContext.datum);
+	lResult = (close_today/close_ndaysago-(double)1)*(double)100;
 	
-	sqlite3_stmt *preparedStatement2;	
-	SQLITE_PREPARE_STATEMENT_TEST(preparedStatement2, sql2, database);
-	SQLITE_STEP_TEST(preparedStatement2);
-	double close_today = sqlite3_column_double(preparedStatement2,0);	// Closing price of current day
-	sqlite3_finalize(preparedStatement2);
-	
-	lResult = (close_today/close_ndaysago-1.)*100.;
-	
-//	std::cout << "ROC:" << lResult << std::endl;
+//	std::cout << "ROC: " << lResult << std::endl;
 }
