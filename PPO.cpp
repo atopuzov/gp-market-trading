90702f15e1015b97bdc7c6e584e2bf23f4b82213
Aleksandar Topuzović <aleksandar.topuzovic@gmail.com>
2011-07-08 16:37:05 +0200

    Percentage Price Oscillator (PPO)

---
+++ PPO.cpp
@@ -0,0 +1,45 @@
+/***************************************************************************
+ * (c) 2008-2011 Aleksandar Topuzović                                      *
+ * <aleksandar.topuzovic@fer.hr>, <aleksandar.topuzovic@gmail.com>         *
+ ***************************************************************************/
+#include "beagle/GP.hpp"
+#include "PPO.hpp"
+#include "ta.hpp"
+
+#include <sstream>
+#include <cmath>
+#include <fstream>
+
+using namespace Beagle;
+
+// Primitive name: PPO
+PPO::PPO() : Beagle::GP::Primitive(0, "PPO")
+{	}
+
+#ifdef BEAGLE_HAVE_RTTI
+
+// Return value type: double
+const std::type_info* PPO::getReturnType(Beagle::GP::Context& ioContext) const
+{
+  return &typeid(Double);
+}
+
+#endif // BEAGLE_HAVE_RTTI
+
+// Exponential Moving Average (PPO)
+// PPO = (12-day EMA - 26-day EMA)/ 26-day EMA * 100
+void PPO::execute(GP::Datum& outDatum, GP::Context& ioContext)
+{
+	Double& lResult = castObjectT<Double&>(outDatum);
+	trading::Context& aContext = castObjectT<trading::Context&>(ioContext);
+	
+	double ema_12_day = ema(aContext.database, aContext.dionica, aContext.datum, 12 );
+	double ema_26_day = ema(aContext.database, aContext.dionica, aContext.datum, 26 );
+//	std::cout << "12day: " << ema_12_day << ", 26day: " << ema_26_day << std::endl;
+
+	lResult = ((ema_12_day - ema_26_day) / ema_26_day) * 100;
+	
+//	std::cout << "PPO: " << lResult << std::endl;
+}
+
+
---
+++ PPO.hpp
@@ -0,0 +1,34 @@
+/***************************************************************************
+ * (c) 2008-2011 Aleksandar Topuzović                                      *
+ * <aleksandar.topuzovic@fer.hr>, <aleksandar.topuzovic@gmail.com>         *
+ ***************************************************************************/
+#ifndef PPO_hpp
+#define PPO_hpp
+
+#include "beagle/GP.hpp"
+#include "sqlite3.h"
+#include "Context.hpp"
+#include <string>
+
+#ifdef BEAGLE_HAVE_RTTI
+#include <typeinfo>
+#endif // BEAGLE_HAVE_RTTI
+
+class PPO : public Beagle::GP::Primitive {
+
+public:
+	typedef Beagle::AllocatorT<PPO,Beagle::GP::Primitive::Alloc> Alloc;
+	typedef Beagle::PointerT<PPO,Beagle::GP::Primitive::Handle> Handle;
+	typedef Beagle::ContainerT<PPO,Beagle::GP::Primitive::Bag> Bag;
+
+	explicit PPO();
+	virtual ~PPO() { }
+
+#ifdef BEAGLE_HAVE_RTTI
+	virtual const std::type_info* getReturnType(Beagle::GP::Context& ioContext) const;
+#endif // BEAGLE_HAVE_RTTI
+
+	virtual void                  execute(Beagle::GP::Datum& outDatum, Beagle::GP::Context& ioContext);
+};
+
+#endif // PPO_hpp
